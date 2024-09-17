d52d28e5b64067f73eb2ed3710997fb936579473
joris.lacassaigne <joris36.lacassaigne@gmail.com>
2024-09-17 15:44:37 +0200

    modification sur l'ajout des commandes

--- commande/models/ligneCommandeModel.js
+++ commande/models/ligneCommandeModel.js
@@ -3,21 +3,6 @@ const db = require("../../database");
 const LigneCommandeModel = {
     // Ajouter des lignes de commande
     addLignesCommande: async (numeroCommande, lignesCommande) => {
-        const sql = `INSERT INTO ligne_commande (reference, quantite_demandee)
-                     VALUES ?`;
-
-        const lignesData = lignesCommande.map((ligne, index) => [
-            numeroCommande,
-            index + 1,  // numéro de ligne
-            ligne.reference,
-            ligne.quantite_demandee
-        ]);
-
-        const result = await db.executeQuery(sql, [lignesData]);
-        return result;
-    },
-
-    /*addLignesCommande: async (numeroCommande, lignesCommande) => {
         const sql = `INSERT INTO ligne_commande (numero, numero_ligne, reference, quantite_demandee)
                      VALUES ?`;
 
@@ -30,7 +15,7 @@ const LigneCommandeModel = {
 
         const result = await db.executeQuery(sql, [lignesData]);
         return result;
-    },*/
+    },
 
     // Récupérer les lignes d'une commande
     getLignesByCommandeId: async (numeroCommande) => {
